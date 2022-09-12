60c34acbd59312eeea04a1fac56809181bc4c951
facugm <39471914+facugm@users.noreply.github.com>
2022-09-11 21:47:01 -0300

    Avances parseo de datos ej 3

--- ejercicio3.pl
+++ ejercicio3.pl
@@ -2,15 +2,14 @@
 use strict;
 use warnings;
 use LWP::UserAgent ();
- 
-my $ua = LWP::UserAgent->new(timeout => 10);
+use JSON;
+use Data::Dumper;
+
+my $ua = LWP::UserAgent->new();
 $ua->env_proxy;
  
 my $response = $ua->get('http://packages.educativa.com/samples/usuarios.json');
- 
-if ($response->is_success) {
-    print $response->decoded_content;
-}
-else {
-    die $response->status_line;
-}
+my $ref = decode_json($response->decoded_content);
+my @users = @{$ref->{usuarios}};
+my %prueba = %{$users[0]};
+print %prueba;
